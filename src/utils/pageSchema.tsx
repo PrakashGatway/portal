6f186f0d5963003dcb015bb8b4f2660bfc483297
gatewayabroadweb-a11y <naveengoswami309@gmail.com>
2026-03-17 11:19:12 +0530

    changes

--- src/utils/pageSchema.tsx
+++ src/utils/pageSchema.tsx
@@ -160,7 +160,8 @@ export const PAGE_TYPES_SCHEMA = {
               { name: "Courses", type: "array", label: "Courses", itemType: "text" },
               { name: "year", type: "text", label: "Year" },
               { name: "icon", type: "text", label: "Icon" },
-              { name: "button", type: "text", label: "Button" },
+              {name : "slug",type: "text", label : "Slug"}
+            
             ],
           },
         ],
@@ -176,56 +177,162 @@ export const PAGE_TYPES_SCHEMA = {
             type: "array",
             label: "Table",
             itemFields: [
-               {
-               name:"iconitem",label : "Icom Item",
+              {
+                name: "iconitem",
+                label: "Icon Item"
               },
               {
-                name : "tableTitle" , type : "array", label : "Table Title",
-                itemFields : [
-                  {name : "tabletitle",type : "text" , label : "Table Title"}
+                name: "tableTitle",
+                type: "array",
+                label: "Table Title",
+                itemFields: [
+                  { name: "tabletitle", type: "text", label: "Table Title" }
                 ]
               },
-              {
-                fields:[
-                  
-                   {name: "tablerow", type: "text", label: "Table Row"},
-                { name: "rowname",label : "row name", type : "array", itemFields:
-                  [
-                    { name: "rowname", type: "text", label: "Program Row Name" }
-                  ]}
 
+              { name: "tablerow", type: "text", label: "Table Row" },
+
+              {
+                name: "tablecolumn1",
+                type: "array",
+                label: "Table Column 1",
+                itemFields: [
+                  {
+                    name: "columnvalue",
+                    type: "text",
+                    label: "Column Value"
+                  }
                 ]
-               
               },
 
               {
-                name: "tablecolumn1", type: "array", label: "Program Column",
-                itemFields:
-                  [
-                    { name: "programname", type: "text", label: "Table Column 1" }
-                  ]
+                name: "tablecolumn2",
+                type: "array",
+                label: "Table Column 2",
+                itemFields: [
+                  {
+                    name: "columnvalue",
+                    type: "text",
+                    label: "Column Value"
+                  }
+                ]
               },
+
               {
-                name: "tablecolumn2", type: "array", label: "Table Column 2",
-                itemFields:
-                  [
-                    { name: "annualtution", type: "text", label: "Annual Tution" }
-                  ]
-              },
-              {
-                name: "tablecolumn3", type: "array", label: "Table Column 3",
-                itemFields:
-                  [
-                    { name: "inr", type: "text", label: "inr" }
-                  ]
+                name: "tablecolumn3",
+                type: "array",
+                label: "Table Column 3",
+                itemFields: [
+                  {
+                    name: "columnvalue",
+                    type: "text",
+                    label: "Column Value"
+                  }
+                ]
               }
             ]
+          },
+          {
+            name: "educationloan",
+            label: "Education Loan",
+            type: "array",
+            itemFields: [
+              { name: "icon", label: "Icon", type: "text" },
+              { name: "title", label: "Title", type: "text" },
+              { name: "educationlist", label: "Education List", type: "text" }
+            ]
+          },
+          {
+            name: "scholarship",
+            label: "Scholarships",
+            type: "array",
+            itemFields: [
+              { name: "icon", label: "Icon", type: "text" },
+              { name: "title", label: "Title", type: "text" },
+              { name: "scholarshiplist", label: "Scholarship List", type: "text" },
+              { name: "scholarshipsublist", label: "Scholarship Sub List", type: "text" },
+              { name: "scholarshiptag", label: "Scholarship List Tag", type: "text" }
 
 
+            ]
           }
         ]
 
 
+      },
+      {
+        type: "UniversityIntakes",
+        label: "University Intakes",
+        fields: [
+          { name: "sectiontitle", label: "Section Title", type: "text" },
+          { name: "sectionsubtitle", label: "Section Sub Title", type: "text" },
+          {
+            name: "intakecards", label: "Intake Cards", type: "array",
+            itemFields: [
+              { name: "cardtitle", label: "Card Title", type: "text" },
+              { name: "cardtag", label: "Card Tag", type: "text" },
+              { name: "cardlist", label: "Card List", type: "text" },
+              { name: "cardfeature", label: "Card Feature University", type: "text" },
+            ]
+          }
+
+        ]
+
+      },
+      {
+        type: "gatewayhelps",
+        label: "Gateway Helps",
+        fields: [
+          { name: "sectiontitle", label: "Section Title", type: "text" },
+          { name: "sectionsubtitle", label: "Section Sub Title", type: "text" },
+          { name: "sectioncontent", label: "Section Content", type: "editor" },
+          {
+            name: "sectioncard", label: "Section Card", type: "array",
+            itemFields: [
+              { name: "cardicon", label: "Card Icon", type: "text" },
+              { name: "cardbadge", label: "Card Badge", type: "text" },
+              { name: "cardtitle", label: "Card Title", type: "text" },
+              { name: "cardsubtitle", label: "Card Sub Title", type: "text" },
+            ]
+          }
+
+        ]
+      },
+      {
+        type : "scholarships",
+        label  :"Scholarships",
+        fields : [
+          {name : "sectiontitle",label : "Section Title",type : "text"},
+          {name : "sectionsubtitle",label : "Section Sub Title",type : "text"},
+          {name : "scholarshipcards",label : "Scholarship Cards",type : "array",
+            itemFields : [
+              {name : "cardbadge" , label : "Card Badge" , type : "text"},
+              {name : "cardtitle" , label : "Card Title" , type : "text"},
+              {name : "cardsubtitle" , label : "Card Sub Title" , type : "text"},
+              {name : "cardtags" , label : "Card Tags" , type : "text"},
+              {name : "slug" , label : "Slug" , type : "text"},
+            ]
+          },
+        ]
+      },
+        {
+        type: "roadmap",
+        label: "Road-map",
+        fields: [
+          { name: "title", type: "text", label: "Section Title" },
+          { name: "subTittle", type: "text", label: "Section Sub Title" },
+          {
+            name: "steps",
+            type: "array",
+            label: "Steps",
+            itemFields: [
+              { name: "name", type: "text", label: "Label" },
+              { name: "content", type: "textarea", label: "Quote" },
+              { name: "Points", type: "textarea", label: "Points" },
+              { name: "icon", type: "text", label: "Icon" }
+            ],
+          },
+        ],
       },
       {
         type: "slider",
