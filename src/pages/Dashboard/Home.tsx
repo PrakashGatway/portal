6585d7392959e05485cc5ed39eb2b63759f4aeb8
PrakashGatway <abroadgateway370@gmail.com>
2026-09-08 10:33:48 +0530

    dd

--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -8,10 +8,6 @@ import AdminDashboard from "./AdminDashbarod";
 
 const EducationAnalytics = () => {
   const { user } = useAuth();
-  const [loading, setLoading] = useState(true);
-  const [analytics, setAnalytics] = useState(null);
-  const [timeRange, setTimeRange] = useState("7d");
-  const [counselors, setAllCounselors] = useState([]);
 
   if (user.role == "teacher") {
     return <TeacherDashboard />;
@@ -20,44 +16,6 @@ const EducationAnalytics = () => {
   if (user.role == "admin") {
     return <AdminDashboard />;
   }
-
-  const fetchAnalytics = async () => {
-    try {
-      setLoading(true);
-      const endpoint =
-        user.role === "Admin"
-          ? "/admin/analytics"
-          : user.role === "Teacher"
-            ? "/teacher/analytics"
-            : "/student/analytics";
-      const response = await api.get(`${endpoint}?range=${timeRange}`);
-      setAnalytics(response.data.data);
-    } catch (error) {
-      console.error("Error fetching analytics:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const fetchCounselors = async () => {
-    try {
-      const res = await api.get("/users?role=counselor");
-
-      setAllCounselors(res.data?.users || []);
-    } catch (error) {
-      console.error("Failed to fetch counselors:", error);
-    }
-  };
-
-  useEffect(() => {
-    if (user?.role == "admin") {
-      fetchCounselors();
-    }
-  }, [user?.role]);
-
-  useEffect(() => {
-    fetchAnalytics();
-  }, [timeRange, user.role]);
 
   if (user.role == "counselor") {
     return <LeadManagement />;
